8c8fa981d6e28b680efcf1a4edeb5e7a370a5d38
Joshua May <git@hotdog.horse>
2024-02-12 13:05:09 +0100

    Add hacky workaround to generate 'required' fields in OpenAPI defs

--- src/slskd/Program.cs
+++ src/slskd/Program.cs
@@ -73,6 +73,66 @@ namespace slskd
     using Utility.CommandLine;
     using Utility.EnvironmentVariables;
     using IOFile = System.IO.File;
+    using Swashbuckle.AspNetCore.SwaggerGen;
+
+// cobbled together from comments on https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/2036
+class RequiredNotNullableSchemaFilter : ISchemaFilter
+{
+    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    {
+        if (schema.Properties == null)
+        {
+            return;
+        }
+
+        FixNullableProperties(schema, context);
+
+        var notNullableProperties = schema
+            .Properties
+            // .Where(x => !x.Value.Nullable && !schema.Required.Contains(x.Key))
+            .Where(x => !x.Value.Nullable && x.Value.Default == default && !schema.Required.Contains(x.Key))
+            .ToList();
+
+        foreach (var property in notNullableProperties)
+        {
+            schema.Required.Add(property.Key);
+        }
+    }
+
+    /// <summary>
+    /// Option "SupportNonNullableReferenceTypes" not working with complex types ({ "type": "object" }), 
+    /// so they always have "Nullable = false",
+    /// see method "SchemaGenerator.GenerateSchemaForMember"
+    /// </summary>
+    private static void FixNullableProperties(OpenApiSchema schema, SchemaFilterContext context)
+    {
+        foreach (var property in schema.Properties)
+        {
+            if (property.Value.Reference != null)
+            {
+                var field = context.Type
+                    .GetMembers(BindingFlags.Public | BindingFlags.Instance)
+                    .FirstOrDefault(x =>
+                        string.Equals(x.Name, property.Key, StringComparison.InvariantCultureIgnoreCase));
+
+                if (field != null)
+                {
+                    var fieldType = field switch
+                    {
+                        FieldInfo fieldInfo => fieldInfo.FieldType,
+                        PropertyInfo propertyInfo => propertyInfo.PropertyType,
+                        _ => throw new NotSupportedException(),
+                    };
+
+                    property.Value.Nullable = !property.Value.Nullable || 
+                        fieldType.IsValueType
+                            ? Nullable.GetUnderlyingType(fieldType) != null
+                            : !field.IsNonNullableReferenceType();
+                }
+            }
+        }
+    }
+}
 
     /// <summary>
     ///     Bootstraps configuration and handles primitive command-line instructions.
@@ -801,6 +861,7 @@ namespace slskd
                 services.AddSwaggerGen(options =>
                 {
                     options.DescribeAllParametersInCamelCase();
+                    options.SchemaFilter<RequiredNotNullableSchemaFilter>();
                     options.SwaggerDoc(
                         "v0",
                         new OpenApiInfo
